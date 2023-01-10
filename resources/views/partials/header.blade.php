1ec0025e1dba9a3fd8407a88597c803f4fea3401
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-01-10 09:37:35 -0500

    [add] basic structure

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,9 +1,9 @@
 <div class="container-fluid">
     <nav class="p-5">
         <ul class="d-flex">
-            <li class="px-5"><a href="">Home</a></li>
-            <li class="px-5"><a href="">About Us</a></li>
-            <li class="px-5"><a href="">Trains</a></li>
+            <li class="px-5"><a href="{{route('home')}}">Home</a></li>
+            <li class="px-5"><a href="{{route('about')}}">About Us</a></li>
+            <li class="px-5"><a href="{{route('trains')}}">Trains</a></li>
         </ul>
     </nav>
 </div>
---
+++ resources/views/trains.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.main')
+
+@section('content')
+    <h1>Trains</h1>
+@endsection
+
+
+@section('title')
+    Trains
+@endsection
