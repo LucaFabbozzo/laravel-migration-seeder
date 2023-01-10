784b55edb70244f59044ba3b415e7747cb8a0c88
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-01-10 09:13:32 -0500

    First Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layout.main')
+
+
+@section('content')
+
+<div class="container">
+    <h1 class="text-center text-uppercase">Home</h1>
+</div>
+
+@endsection
+
+
+@section('title')
+  Home
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,9 @@
+<div class="container-fluid">
+    <nav class="p-5">
+        <ul class="d-flex">
+            <li class="px-5"><a href="">Home</a></li>
+            <li class="px-5"><a href="">About Us</a></li>
+            <li class="px-5"><a href="">Trains</a></li>
+        </ul>
+    </nav>
+</div>
