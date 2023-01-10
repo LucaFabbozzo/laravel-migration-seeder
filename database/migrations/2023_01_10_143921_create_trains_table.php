5ecce628b92b31b73c6656208e7efbb4c4aeb2c6
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-01-10 09:58:48 -0500

    start to create the migration

---
+++ database/migrations/2023_01_10_143921_create_trains_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            // Azienda
+            // Stazione di partenza
+            // Stazione di arrivo
+            // Orario di partenza
+            // Orario di arrivo
+            // Codice Treno
+            // Numero Carrozze
+            // In orario
+            // Cancellato
+            $table->string('azienda', 15);
+            $table->string('stazione_di_partenza');
+            $table->dateTime('orario_di_partenza', $precision = 0);
+            $table->dateTime('orario_di_arrivo', $precision = 0);
+            $table->string('codice_treno', 10);
+            $table->tinyInteger('numero_carrozze')->unsigned();
+            $table->boolean('in_orario');
+            $table->boolean('cancellato');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
